348a796a960e1ced1c978499aecaf5caa847ca07
Ericson <ehuamani.dev@gmail.com>
2019-02-27 13:58:40 -0500

    Corrección de Array - FichaSchema

--- sisproi-backend/models/ficha.js
+++ sisproi-backend/models/ficha.js
@@ -8,8 +8,8 @@ var Schema = mongoose.Schema;
 
 var FichaSchema = new Schema({
     sector_nivel_1: { type: String, required: [true, 'Sector nivel 1 es requerido'] },
-    sector_nivel_2: { type: Array, required: [true, 'Sector nivel 2 es requerido'] },
-    sector_nivel_3: { type: Array, required: [true, 'Sector nivel 3 es requerido'] },
+    sector_nivel_2: [{ type: String }],
+    sector_nivel_3: [{ type: String }],
     jurisdiccion: { type: String, required: [true, 'Jurisdiccion es requerido'] },
     jurisdiccion_otro: { type: String },
     nombre_proyecto: { type: String, required: [true, 'Nombre del proyecto es requerido'], unique: true },
