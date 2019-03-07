8efbbfb0869a2a2d2292a8ac461e12b2cbc9bf09
LuiggiVargas <luiggivargas096@gmail.com>
2019-03-07 16:42:03 -0500

    Registro-luiggi

--- sisproi/src/app/services/usuario/usuario.service.ts
+++ sisproi/src/app/services/usuario/usuario.service.ts
@@ -18,7 +18,6 @@ export class UsuarioService {
   login(usuario: Usuario, recordar: boolean = false) {
     let url = env.URI_API.concat('login');
     return this.http.post<any>(url, usuario)
-
   }
 
   //servicio para crear usuario
@@ -26,22 +25,36 @@ export class UsuarioService {
     let url = env.URI_API.concat('usuario');
     return this.http.post(url, usuario)
   }
-  //servicio para mostrar usuario
+
+  //servicio para mostrar todos los usuarios
   allUsuario(){
     let url = env.URI_API.concat('usuario');
     return this.http.get<any>(url)
   }
 
+ //servicio para mostrar usuarios excepto el que esta en uso
+  ListaUsuarios(id){
+    let url = env.URI_API.concat('usuario'+`/${id}`);
+    return this.http.post<any>(url,null)
+  }
+ //servicio para comprobar duplicidad de correo
+  duplicidadCorreo(correo){
+    let url = env.URI_API.concat('usuario'+`/${correo}`);
+    return this.http.post<any>(url,null)
+  }
+
+ //servicio para mostrar un usuario
   selectOne(id){
     let url = env.URI_API.concat('usuario'+`/${id}`);
     return this.http.get<any>(url)
   }
 
-  //servicio para mostrar usuario
+//servicio para eliminar usuario
   delete(id){
     let url = env.URI_API.concat('usuario'+ `/${id}`);
     return this.http.delete<any>(url)
   }
+
 //servicio para actualizar usuario
   updateUsuario(id,updateusuario:Usuario){
     let url = env.URI_API.concat('usuario'+ `/${id}`);
