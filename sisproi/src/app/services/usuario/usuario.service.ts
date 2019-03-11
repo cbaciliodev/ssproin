12710a8bd6195c33becfaaa0d84cf62a004424f1
ehuamani.dev <ehuamani.dev@gmail.com>
2019-03-11 15:41:47 -0500

    Cambios Dashboard - Ericson

--- sisproi/src/app/services/usuario/usuario.service.ts
+++ sisproi/src/app/services/usuario/usuario.service.ts
@@ -8,54 +8,58 @@ import { HttpClient } from '@angular/common/http';
 })
 export class UsuarioService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  userLogin(): Usuario {
+    return JSON.parse(localStorage.getItem('usuario'));
+  }
 
   estaLogeado() {
     return localStorage.getItem('token') ? true : false;
   }
 
-  //servicio para consultar usuario
+  // servicio para consultar usuario
   login(usuario: Usuario) {
     let url = env.URI_API.concat('login');
     return this.http.post<any>(url, usuario)
   }
 
-  //servicio para crear usuario
+  // servicio para crear usuario
   crearUsuario(usuario: Usuario) {
     let url = env.URI_API.concat('usuario');
     return this.http.post(url, usuario)
   }
 
-  //servicio para mostrar todos los usuarios
+  //s ervicio para mostrar todos los usuarios
   allUsuario(){
     let url = env.URI_API.concat('usuario');
     return this.http.get<any>(url)
   }
 
- //servicio para mostrar usuarios excepto el que esta en uso
+ // servicio para mostrar usuarios excepto el que esta en uso
   ListaUsuarios(id){
     let url = env.URI_API.concat('usuario'+`/${id}`);
     return this.http.post<any>(url,null)
   }
- //servicio para comprobar duplicidad de correo
+ // servicio para comprobar duplicidad de correo
   duplicidadCorreo(correo){
     let url = env.URI_API.concat('usuario'+`/${correo}`);
     return this.http.post<any>(url,null)
   }
 
- //servicio para mostrar un usuario
+ // servicio para mostrar un usuario
   selectOne(id){
     let url = env.URI_API.concat('usuario'+`/${id}`);
     return this.http.get<any>(url)
   }
 
-//servicio para eliminar usuario
+// servicio para eliminar usuario
   delete(id){
     let url = env.URI_API.concat('usuario'+ `/${id}`);
     return this.http.delete<any>(url)
   }
 
-//servicio para actualizar usuario
+// servicio para actualizar usuario
   updateUsuario(id,updateusuario:Usuario){
     let url = env.URI_API.concat('usuario'+ `/${id}`);
     return this.http.put<any>(url,updateusuario)
